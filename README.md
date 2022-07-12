62333f1e306c4bfd881f0e0b6b3140ae78c9bd30
Suraj K Thapa <sjkrta@gmail.com>
2022-07-12 21:24:10 +1000

    final commit

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
+<!-- how to run this next app -->
+
 ```bash
 npm run dev
 # or
 yarn dev
 ```
+
+<!-- static vs server-side rendering -->
+
+Static      => Generates HTML at build time
+Server-Side => Generates HTML on each request
--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../../styles/utils.module.css';
-import Link from 'next/link';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-const name = 'Suraj K Thapa';
-export const siteTitle = 'Blog Site';
+const name = 'Suraj K Thapa'
+export const siteTitle = 'Sjkrta'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,13 +14,13 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Blogs, Youtube videos tutorial links."
+          content="Learn how to build a personal website using Next.js"
         />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -69,5 +69,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  );
+  )
 }
